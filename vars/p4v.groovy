73d0d5b0e31336b8e8100237511cac0d19b2e3fb
Rick R <rick.rijkers1@gmail.com>
2021-12-08 15:09:07 +0100

    Made dir a parameter

--- vars/p4v.groovy
+++ vars/p4v.groovy
@@ -59,10 +59,10 @@ def getChangelistDescr(id)
    return desc
 }
 
-def getShelvedChangelists()
+def getShelvedChangelists(dir)
 {
    def p4s = p4(credential: p4Info.credential, workspace: manualSpec(charset: 'none', cleanup: false, name: p4Info.workspace, pinHost: false, spec: clientSpec(allwrite: true, backup: true, changeView: '', clobber: false, compress: false, line: 'LOCAL', locked: false, modtime: false, rmdir: false, serverID: '', streamName: '', type: 'WRITABLE', view: p4Info.viewMapping)))
-   def changeLists = p4s.run('changes', '-s', 'shelved','//...')
+   def changeLists = p4s.run('changes', '-s', 'shelved',"${dir}")
    return changeLists
 }
 
