61396d661519d406a811c8e84bd7858ca9e0c6a9
Rick R <rick.rijkers1@gmail.com>
2021-12-15 13:40:58 +0100

    Update p4v.groovy

--- vars/p4v.groovy
+++ vars/p4v.groovy
@@ -66,6 +66,13 @@ def getShelvedChangelists(dir)
    return changeLists
 }
 
+def getShelvedChangelists(dir,numberOfChangelists)
+{
+   def p4s = p4(credential: p4Info.credential, workspace: manualSpec(charset: 'none', cleanup: false, name: p4Info.workspace, pinHost: false, spec: clientSpec(allwrite: true, backup: true, changeView: '', clobber: false, compress: false, line: 'LOCAL', locked: false, modtime: false, rmdir: false, serverID: '', streamName: '', type: 'WRITABLE', view: p4Info.viewMapping)))
+   def changeLists = p4s.run('changes', '-s', 'shelved','-m',"${numberOfChangelists}","${dir}")
+   return changeLists
+}
+
 def getCurrChangelistDescr()
 {
    return getChangelistDescr(env.P4_CHANGELIST)
